0b3a6850e063664dfee084dbf4e0896a82e1b122
jterstall <jeroen_t@hotmail.com>
2016-10-27 18:16:49 +0200

    add json files to elasticsearch python

---
+++ populateElasticsearch.py
@@ -0,0 +1,17 @@
+from elasticsearch import  helpers
+from elasticsearch import Elasticsearch
+import json
+import os
+
+
+if __name__ == '__main__':
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    
+    # add bulk to elasticsearch
+    json_folder = '/home/neoray/Documents/Zoekmachines/Week 8/telegraaf-json/'
+
+    for filename in os.listdir(json_folder):
+        if filename.endswith('.json'):
+            print(filename)
+            with open(json_folder + filename) as infile:
+                helpers.bulk(es, json.load(infile))
